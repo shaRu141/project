a8bba3c8077a2ef3060463990e86232e62baedd3
shaRu141 <109732339+shaRu141@users.noreply.github.com>
2022-07-21 15:42:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Microsoft</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">PhotoGallery</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+   <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+     
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categories
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">trainings we offer</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">feedbacks</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Contact Us</a>
+        </div>
+        <li class="nav-item">
+      <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      
+    </ul>
+     
+  </div>
+</nav>
+
+
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/wallpapers hd dragon ball z   ALOjamiento de IMgenes.jpg" alt="DrazonBall">
+    </div>
+
+    <section class="my-4">
+        <div class="py-4">
+            <h2 class="text-center">DBZ</h2> 
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/Dragon Logo Designs HD Wallpapers Download Wallpapers in HD.jpg" class="img-fluid pb-3"
+        
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/Dragon Logo Designs HD Wallpapers Download Wallpapers in HD.jpg" class="img-fluid pb-3"
+    </div>
+    <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/Dragon Logo Designs HD Wallpapers Download Wallpapers in HD.jpg" class="img-fluid pb-3"
+    </div>
+    </section>
+    
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
